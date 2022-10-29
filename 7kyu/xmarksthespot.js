705163e367ce605bf8ab507283727d4cb179b939
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2022-10-29 17:44:26 -0300

    new kata

---
+++ 7kyu/xmarksthespot.js
@@ -0,0 +1,57 @@
+// Task:
+// Given a two dimensional array, return the co-ordinates of x.
+
+// If x is not inside the array, or if x appears multiple times, return [].
+
+// The co-ordinates should be zero indexed in row-major order.
+// You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+// Examples
+// Input: []
+// Return an empty array if input is an empty array => []
+
+// Input: [
+//   ['o', 'o'],
+//   ['o', 'o']
+// ]
+// Return an empty array if no x found => []
+
+// Input: [
+//   ['x', 'o'],
+//   ['o', 'x']
+// ]
+// Return an empty array if more than one x found => []
+
+// Input: [
+//   ['x', 'o'],
+//   ['o', 'o']
+// ]
+// Return [0,0] when x at top left => [0, 0]
+
+// Input: [
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+// ]
+// Return [4,6] for the example above => [4, 6]
+
+const xMarksTheSpot = (input) => {
+  let count = 0;
+  let x = 0;
+  let y = 0;
+  input.map((value, index) => {
+    if (value.includes("x")) {
+      value.map((x, index) => (x === "x" ? (y = index) : value));
+
+      count++;
+
+      x = index;
+    } else {
+      return value;
+    }
+  });
+  return count === 0 ? [] : count > 1 ? [] : [x, y];
+};
