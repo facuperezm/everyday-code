4a0b0719cc5196fae55bd9c5d9d94e2557e5cdaf
facuperezm <facundoperezmontalvo@gmail.com>
2023-01-14 15:25:14 -0300

    new kata

--- 8kyu/csvRep.js
+++ 8kyu/csvRep.js
@@ -1,21 +1,21 @@
-Create a function that returns the CSV representation of a two-dimensional numeric array.
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
 
-Example:
+// Example:
 
-input:
-   [[ 0, 1, 2, 3, 4 ],
-    [ 10,11,12,13,14 ],
-    [ 20,21,22,23,24 ],
-    [ 30,31,32,33,34 ]] 
-    
-output:
-     '0,1,2,3,4\n'
-    +'10,11,12,13,14\n'
-    +'20,21,22,23,24\n'
-    +'30,31,32,33,34'
-Array's length > 2.
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]]
 
-More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+// Array's length > 2.
+
+// More details here: https://en.wikipedia.org/wiki/Comma-separated_values
 
 function toCsvText(array) {
   let result = "";
