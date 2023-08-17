6626c6b0b02d29f2204b4c6ceaa80b658e584ee2
facuperezm <facundoperezmontalvo@gmail.com>
2023-08-17 16:47:30 -0300

    new kata

---
+++ 6kyu/ArrayHelpers.js
@@ -0,0 +1,35 @@
+// This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+
+// Explanation:
+
+// square() must return a copy of the array, containing all values squared
+// cube() must return a copy of the array, containing all values cubed
+// average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+// sum() must return the sum of all array values
+// even() must return an array of all even numbers
+// odd() must return an array of all odd numbers
+// Note: the original array must not be changed in any case!
+
+Array.prototype.square = function () {
+	return this.map(num => num * num)
+}
+
+Array.prototype.cube = function () {
+	return this.map(num => num * num * num)
+}
+
+Array.prototype.average = function () {
+	return this.reduce((acc, curr) => acc + curr, 0) / this.length
+}
+
+Array.prototype.sum = function () {
+	return this.reduce((acc, curr) => acc + curr, 0)
+}
+
+Array.prototype.even = function () {
+	return this.filter(num => num % 2 === 0)
+}
+
+Array.prototype.odd = function () {
+	return this.filter(num => num % 2 !== 0)
+}
