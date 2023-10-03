9fff610e11eaa4716072fc49c67870dd950afa8b
facuperezm <facundoperezmontalvo@gmail.com>
2023-10-03 15:10:04 -0300

    new kata

---
+++ 8kyu/defineACardSuit.js
@@ -0,0 +1,24 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+  const suit = card.charAt(card.length - 1);
+  switch (suit) {
+    case "♣":
+      return "clubs";
+    case "♦":
+      return "diamonds";
+    case "♥":
+      return "hearts";
+    case "♠":
+      return "spades";
+    default:
+      return "unknown";
+  }
+}
