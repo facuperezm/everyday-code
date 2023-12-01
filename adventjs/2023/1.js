adfb05414034f4b49159d7210f08bf8008b0c8e8
facuperezm <facundoperezmontalvo@gmail.com>
2023-12-01 16:51:54 -0300

    adventjs

---
+++ adventjs/2023/1.js
@@ -0,0 +1,13 @@
+function findFirstRepeated(gifts) {
+  const seen = new Set();
+
+  for (const gift of gifts) {
+    if (seen.has(gift)) {
+      return gift;
+    }
+
+    seen.add(gift);
+  }
+
+  return -1;
+}
