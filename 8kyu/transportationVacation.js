208250bb587cc5a408caa990606c89e0e26f1a36
facuperezm <facundoperezmontalvo@gmail.com>
2024-08-09 17:15:22 -0300

    new kata

---
+++ 8kyu/transportationVacation.js
@@ -0,0 +1,16 @@
+// Hi Helen,
+
+// Thank you so much for considering me for this opportunity and for providing such detailed information about the project. I appreciate the offer, but I wanted to let you know that after my current time with EverWeb ends, I will be moving on to work with another company. Therefore, I won’t be able to continue with SUP.
+
+// Thank you again for thinking of me
+
+function rentalCarCost(d) {
+  let total = d * 40;
+  if (d >= 7) {
+    return total - 50;
+  } else if (d >= 3) {
+    return total - 20;
+  } else {
+    return total;
+  }
+}
