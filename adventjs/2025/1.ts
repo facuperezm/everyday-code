ed1ec9c0bb3cfa10a7696736d2ae7edee67a9dd7
facuperezm <facundoperezmontalvo@gmail.com>
2025-12-01 14:34:58 -0300

    adventjs day 1

---
+++ adventjs/2025/1.ts
@@ -0,0 +1,23 @@
+// Santa has received a list of gifts, but some are defective. A gift is defective if its name contains the # character.
+
+// Help Santa by writing a function that takes a list of gift names and returns a new list that only contains the non-defective gifts.
+
+// Examples
+// const gifts1 = ['car', 'doll#arm', 'ball', '#train']
+// const good1 = filterGifts(gifts1)
+// console.log(good1)
+// // ['car', 'ball']
+
+// const gifts2 = ['#broken', '#rusty']
+// const good2 = filterGifts(gifts2)
+// console.log(good2)
+// // []
+
+// const gifts3 = []
+// const good3 = filterGifts(gifts3)
+// console.log(good3)
+// // []
+
+function filterGifts(gifts: string[]): string[] {
+  return gifts.filter((gift: string) => !gift.includes("#"))
+}
